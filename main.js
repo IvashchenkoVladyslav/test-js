e75fdae1c391f81e869d0d2aa25a94887c0a6b45
Vladyslav-Ivashchenko <ivu2603@gmail.com>
2023-03-04 18:29:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-const inShoud = confirm('Будеш їбатись в сраку?')
-console.log(inShoud);
+// let numberDickInAss = "150pie";
+// numberDickInAss = Number.parseInt(numberDickInAss);
+// console.log("Кількіть хуїв запханих в сраку:", numberDickInAss);
+
+// let salaryInSuckDick = 16578.6548754;
+// salaryInSuckDick = Number(salaryInSuckDick.toFixed(3));
+// console.log("Зарплата за виконану роботу:", salaryInSuckDick);
+
+let fit = prompt("Введи число");
+fit = Number(fit);
+
+console.log(fit);
+
+let flip = prompt("Введи степінь обчислення");
+flip = Number(flip);
+console.log(flip);
+
+const result = fit ** flip;
+
+console.log(result) ;
